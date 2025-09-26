0cee77a2036b99d2e420b7080918310e13729190
shahanimetharath <ayshashahani15@gmail.com>
2025-09-26 17:38:32 +0200

    inital commit

---
+++ src/main/kotlin/org/codefacory/urlshortner/UrlShortnerApplication.kt
@@ -0,0 +1,12 @@
+package org.codefacory.urlshortner
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class UrlShortnerApplication
+
+fun main(args: Array<String>) {
+    runApplication<UrlShortnerApplication>(*args)
+    println("hello world!")
+}
---
+++ src/test/kotlin/org/codefacory/urlshortner/UrlShortnerApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.codefacory.urlshortner
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class UrlShortnerApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
